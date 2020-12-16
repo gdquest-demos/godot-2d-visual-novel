3f1f3f4e325e5b90c4a92a3ba60af5084a8c8e64
Nathan Lovato <nathan@gdquest.com>
2020-12-15 18:56:47 -0600

    Add project with text box
    Supports animated text display and displaying an arrow to continue

---
+++ godot/TextBox.gd
@@ -0,0 +1,49 @@
+## Displays character replies in a dialogue
+extends Control
+
+## Emitted when all the text finished displaying.
+signal display_finished
+
+## Speed at which the characters appear in the text body in characters per second.
+export var display_speed := 20.0
+export var bbcode_text := "" setget set_bbcode_text
+
+onready var _name_label: Label = $NameLabel
+onready var _rich_text_label: RichTextLabel = $RichTextLabel
+onready var _tween: Tween = $Tween
+onready var _blinking_arrow: Control = $BlinkingArrow
+
+
+func _ready() -> void:
+	display("Hello, world!", "Dan")
+
+
+func display(text: String, character_name := "", speed := display_speed) -> void:
+	set_bbcode_text(text)
+
+	if character_name != "":
+		_name_label.text = character_name
+
+	if speed != display_speed:
+		display_speed = speed
+
+
+func set_bbcode_text(text: String) -> void:
+	bbcode_text = text
+	if not is_inside_tree():
+		yield(self, "ready")
+	_rich_text_label.bbcode_text = bbcode_text
+
+	# Required for the `_rich_text_label`'s  text to update and the code below to work.
+	yield(get_tree(), "idle_frame")
+
+	var character_count := _rich_text_label.get_total_character_count()
+	_tween.interpolate_property(
+		_rich_text_label, "visible_characters", 0, character_count, character_count / display_speed
+	)
+	_tween.start()
+
+
+func _on_Tween_tween_all_completed() -> void:
+	emit_signal("display_finished")
+	_blinking_arrow.visible = true
